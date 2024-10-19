f90ee4dc9d16f8938c9f30cb7824f06eb931e2f8
shubham <shubhsawant4751@gmail.com>
2024-10-20 00:25:23 +0530

    Adding Stored Procedure Script file

--- Megaminds_technologies/Models/User.cs
+++ Megaminds_technologies/Models/User.cs
@@ -7,14 +7,14 @@ namespace Megaminds_technologies.Models
 {
 	public class User
 	{
-        public int Id { get; set; }
-        public int StateId { get; set; }
+		public int Id { get; set; }
+		public int StateId { get; set; }
 		public int CityId { get; set; }
 		public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-        public string StateName { get; set; }
-        public string CityName {  get; set; }
-    }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public string Address { get; set; }
+		public string StateName { get; set; }
+		public string CityName { get; set; }
+	}
 }
